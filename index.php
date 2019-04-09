bfb2d30f4e01b4481d937d25ac65559063905d12
Hans-Peter Dorn <hans-peter.dorn@t-online.de>
2019-04-09 17:02:17 +0200

    Update index.php
    ImageDescription can be displayed (instead of an external file)
    	(Some text have been translated to German, CSS files and fonts are loaded from subdirs)

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 
 <html>
 
@@ -6,84 +6,56 @@
 	Author: Dmitri Popov
 	License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt
 	Source code: https://github.com/dmpop/mejiro
+
+	19/03/02 - This code has been adapted in order to incorporaste PAGINATION
+	See // NEW for changes
+	9/4/2019 - ImageDescription can be displayed (instead of an external file)
+	(Some text have been translated to German, CSS files and fonts are loaded from subdirs)
 -->
 
 	<head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-	<meta name="viewport" content="width=device-width">
-	<link href='http://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Harmattan' rel='stylesheet' type='text/css'>
-	<script src="https://use.fontawesome.com/b4c062efea.js"></script>
-	<link rel="shortcut icon" href="favicon.ico" />
-
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta name="description" content="Bilder zur Europakarte">
+        <meta name="keywords" content="Bilder, Reise, Europa">
+        <meta name="author" content="Hans-Peter Dorn">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="robots" content="index,follow">
+		<link href='css/fotogrid.css' rel='stylesheet' type='text/css'>
+		<link href='css/font-awesome.min.css' rel='stylesheet' type='text/css'>
+		<link rel="shortcut icon" href="../../favicon.ico" />
+        <title>Europakarte</title>
 	<?php
-
+        
 	// User-defined settings
-	$title = "目白 Mejiro";
-	$tagline = "Responsive single-file open source photo grid";
-	$columns = 4; // Specify the number of columns in the grid layout (2, 3, or 4)
-	$footer="<a style='color: white' href='http://dmpop.github.io/mejiro/'>Mejiro</a> &mdash; pastebin for your photos";
-	$photo_dir = "photos"; // Directory for storing photos
-	$r_sort = false;	// Set to true to show tims in the reverse order (oldest ot newest)
-	$google_maps = false;	// Set to true to use Google Maps instead of OpenStreetMap
-	$use_shortLink = true; // Set to false if you do not want to use short URLs or is.gd is inaccessible at your location
-	// Change this next line if you wish to use a different short URL provider (bit.ly, goo.gl, mcaf.ee)
-	$shortLink_API = "https://is.gd/create.php?format=simple&url=http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	$links = true;	// Enable the link box
+
+	$title = "Bilder der Europakarte"; 						//Titel der Fotoseite
+	$tagline = "Tour de Europe";							//Beschreibung der Seite
+	$columns = 4; 											//Anzahl der Spalten für das Seitenlayout (2, 3, oder 4)
+	$per_page = 100; 										//NEW - Number of images per page for pagination
+	$footer="<a style='color: white' href='https://hapede.de'>Homepage</a>";
+	$photo_dir = "photos"; 									// Directory for storing photos
+	$r_sort = false;										// Set to true to show tims in the reverse order (oldest ot newest)
+	$google_maps = false;									// Set to true to use Google Maps instead of OpenStreetMap
+	$links = false;	// Enable the link box
 	// If the link box is enabled, specify the desired links and their icons in the array below
-	$links = array (
-	array('https://www.eyeem.com/u/dmpop','fa fa-instagram fa-lg'),
-	array('https://scribblesandsnaps.com/','fa fa-wordpress fa-lg'),
-	array('https://github.com/dmpop','fa fa-github fa-lg')
-	);
+	//	$links = array (
+	//	array('https://www.eyeem.com/u/dmpop','fa fa-instagram fa-lg'),
+	//	array('https://scribblesandsnaps.com/','fa fa-wordpress fa-lg'),
+	//	array('https://github.com/dmpop','fa fa-github fa-lg')
+	//	);
 	$raw_formats = '.{ARW,arw,NEF,nef,ORF,orf,CR2,cr2,PNG,png}'; // Supported RAW formats. Add other formats, if needed.
 	?>
-
-	<style>
-		body { font-family: 'Fira Sans', sans-serif; font-size: 2.0vh; text-align: justify; background-color: #303030; }
-		a { color: #999; }
-		a.superscript { position: relative; top: -0.7em; font-size: 51%; text-decoration: none; }
-		h1 { color: #e3e3e3; font-family: 'Harmattan', sans-serif; font-size: 7.0vh; font-weight: 400; text-align: center; margin-top: 0.3em; margin-bottom: 0.5em; line-height: 100%; letter-spacing: 1px; }
-		h2 { color: #e3e3e3; font-family: 'Harmattan', sans-serif; font-size: 3.0vh; font-weight: 400; text-align: center; margin-top: 1em; margin-bottom: 0.5em; line-height: 100%; letter-spacing: 1 }
-		h3 { color: #e3e3e3; font-family: 'Harmattan', sans-serif; font-size: 2.0vh; font-weight: 400; text-align: center; margin-top: 1em; margin-bottom: 0.5em; line-height: 100%; letter-spacing: 1 }
-		p { font-size: 2.0vh; text-align: left; }
-		p.msg { margin-left: auto; margin-right: auto; margin-bottom: 0px; margin-top: 0.5em; border-radius: 5px; width: auto; border-width: 1px; font-size: 2.0vh; letter-spacing: 3px; padding: 5px; color: #ffffff; background: #3399ff; text-align: center; width:500px; }
-		p.caption { border-style: none; border-width: 1px; font-size: 2.0vh; padding: 5px; color: #303030 !important; margin-bottom: 0px; margin-left: auto; margin-right: auto; line-height: 2.0em; text-align: center; }
-		p.box { width: auto; border-style: dotted; border-width: 1px; font-size: 2.0vh; padding: 5px; color: #e3e3e3; margin-bottom: 0px; margin-left: auto; margin-right: auto; line-height: 2.0em; text-align: center; }
-		#content { color: #e3e3e3; }
-		.text { text-align: center; padding: 0px; color: inherit; float: left; }
-		.center { font-size: 2.0vh; padding: 1px; height: auto; text-align: center; padding: 0px; margin-bottom: 2em; }
-		.footer { line-height: 3.0em; text-align: center; font-family: monospace; font-size: 1.5vh; position:fixed; left:0px; bottom:0px; height:3em; width:100%; background:#3973ac; }
-		/* Responsive grid based on http://alijafarian.com/responsive-image-grids-using-css/ */
-		ul.rig { list-style: none; font-size: 0px; margin-left: -5.6%; /* should match li left margin */ }
-		ul.rig li { display: inline-block; padding: 10px; margin: 0 0 2.5% 2.5%; background: #fff; font-size: 16px; font-size: 1rem; vertical-align: top; box-sizing: border-box; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; }
-		ul.rig li img { max-width: 100%; height: auto; }
-		ul.rig li h3 { margin: 0 0 1px; }
-		ul.rig li p { font-size: .9em; line-height: 2.0em; color: #999; }
-		/* class for 1 column */
-	ul.rig.column-1 li { width: 43.3%; /* this value + 2.5 should = 50% */ }
-			/* class for 2 columns */
-		ul.rig.columns-2 li { width: 47.5%; /* this value + 2.5 should = 50% */ }
-		/* class for 3 columns */
-		ul.rig.columns-3 li { width: 30.83%; /* this value + 2.5 should = 33% */ }
-		/* class for 4 columns */
-		ul.rig.columns-4 li { width: 22.5%; /* this value + 2.5 should = 25% */ }
-		@media (max-width: 480px) {
-			ul.grid-nav li { display: block; margin: 0 0 5px; }
-			ul.grid-nav li a { display: block; }
-			ul.rig { margin-left: 0; }
-			ul.rig li { width: 100% !important; /* over-ride all li styles */ margin: 0 0 20px; }
-			* { margin: 0; padding: 0; }
-	</style>
 
 	<?php
 
 	// Detect browser language
+
 	$language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
 	// The $d parameter is used to detect a subdirectory
+
 	// basename and str_replace are used to prevent the path traversal attacks. Not very elegant, but it should do the trick.
-        $sub_photo_dir = basename($_GET['d']).DIRECTORY_SEPARATOR;
+    $sub_photo_dir = basename($_GET['d']).DIRECTORY_SEPARATOR;
 	$photo_dir = str_replace(DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, $photo_dir.DIRECTORY_SEPARATOR.$sub_photo_dir);
 
 	/*
@@ -135,13 +107,15 @@
 
 	// Check whether the required directories exist
 		if (!file_exists($photo_dir) || !file_exists($photo_dir.'tims')) {
-		exit ('<p class="msg"><u>'.$photo_dir. '</u> or <u>'. $photo_dir.'tims</u> directory doesn\'t exist. You must create it manually. <a href="'.basename($_SERVER['PHP_SELF']).'">Back</a></p>');
+		exit ('<p class="msg"><u>'.$photo_dir. '</u> or <u>'. $photo_dir.'tims</u> directory doesn\'t exist. You must create it manually. <a href="'.basename($_SERVER['PHP_SELF']).'">Zurück</a></p>');
 
 	}
 
 	// Get file info
 	$files = glob($photo_dir.'*.{jpg,jpeg,JPG,JPEG}', GLOB_BRACE);
 	$fileCount = count($files);
+	
+	// Thumbnails erzeugen
 
 	function createTim($original, $tim, $timWidth)
 	{
@@ -202,7 +176,42 @@
 	echo "</head>";
 	echo "<body>";
 	echo "<div id='content'>";
-
+		
+		
+	//NEW - Preparing pagination - Calculate total items per page * START
+	$filetype = '*.*';
+	$files = glob($photo_dir.$filetype);
+	$total = count($files);
+	$last_page = ceil($total / $per_page);
+	if (isset($_GET["photo"]) == '')
+	{
+	if(isset($_GET["page"]) && ($_GET["page"] <=$last_page) && ($_GET["page"] > 0) && ($_GET["allimages"] != 1) )
+	{
+		$page = $_GET["page"];
+		$offset = ($per_page + 1)*($page - 1); 
+		echo "Seite ".$_GET["page"]." von ".$last_page." (max. ".$per_page." Fotos je Seite)"." - ".$fileCount." images";
+		echo '&nbsp&nbsp&nbsp<a style="color: yellow;" href="/index.php?allimages=1">Zeige alle Fotos</a>';
+	}
+	else
+	{
+		if(isset($_GET["allimages"]) != 1)
+		{
+		echo "Seite 1 von ".$last_page." (max. ".$per_page." Fotos je Seite)"." - ".$fileCount." images";
+		echo '&nbsp&nbsp&nbsp<a style="color: yellow;" href="/index.php?allimages=1">Zeige alle Fotos</a>';
+		}
+		$page=1;
+		$offset=0;
+	}
+	if (isset($_GET['allimages']) == 1)
+			{$allimages = 1;}
+	}
+	$max = $offset + $per_page;
+	if($max>$total)
+	{
+		$max = $total; 
+	}
+//NEW - Preparing pagination - Calculate total items per page * END
+		
 	// The $grid parameter is used to show the main grid
 	$grid = (isset($_GET['photo']) ? $_GET['photo'] : null);
 	if (!isset($grid)) {
@@ -210,16 +219,75 @@
 		echo "<div class ='center'>".$tagline."</div>";
 		echo "<ul class='rig columns-".$columns."'>";
 
-		for ($i=($fileCount-1); $i>=0; $i--) {
+			if ($allimages == 1)
+			{
+			for ($i=($fileCount-1); $i>=0; $i--) { //OLD LINE
+			//for($i = $offset; $i< $max; $i++){ //NEW - Pagination
 			$file = $files[$i];
 			$tim = $photo_dir.'tims/'.basename($file);
 			$filepath = pathinfo($file);
-			echo '<li><a href="index.php?photo='.$file.'&d='.$sub_photo_dir.'"><img src="'.$tim.'" alt="'.$filepath['filename'].'" title="'.$filepath['filename'].'"></a><h3>'.$filepath['filename'].'</h3></li>';
-		}
+			// 	HPD: Auslesen der EXIF Daten zur Anzeige des Titels		
+			$exif = exif_read_data($file, 0, true);
+			$titel = $exif["IFD0"]["ImageDescription"];
+			echo '<li><a href="index.php?photo='.$file.'&d='.$sub_photo_dir.'"><img src="'.$tim.'" alt="'.$filepath['filename'].'" title="'.$filepath['filename'].'"></a><h3>'.$titel.'</h3></li>';
+			}
+			}
+			else
+				{
+			//for ($i=($fileCount-1); $i>=0; $i--) { //OLD LINE
+			for($i = $offset; $i< $max; $i++){ //NEW - Pagination
+			if($r_sort) {
+			rsort($files);
+			}
+			$file = $files[$i];
+			$tim = $photo_dir.'tims/'.basename($file);
+			$filepath = pathinfo($file);
+			$exif = exif_read_data($file, 0, true);
+			$titel = $exif["IFD0"]["ImageDescription"];
+			echo '<li><a href="index.php?photo=' . $file . '&d=' . $sub_photo_dir . '"><img src="' . $tim . '" alt="' . $filepath['filename'] . '" title="' . $filepath['filename'] . '"></a><h3>'.$titel.'</h3></li>';
+			}
+			}
+			
 		echo "</ul>";
 	}
+		
+		if(isset($_GET["allimages"]) != 1)
+		
+		{
+		show_pagination($page, $last_page); //NEW - Pagination - Show navigation on bottom of page
+		}
+	
+	
+	//NEW - Using the following function you can create the navigation links * START
 
+	function show_pagination($current_page, $last_page)
+		{
+			echo '<div class="center">';
+			if( $current_page != 1 && isset($_GET["photo"]) == ''  )
+				{
+					echo '<a style="color: #e3e3e3;" href="?page='."1".'"&nbsp> Erste Seite</a>&nbsp&nbsp&nbsp';
+				}
+			if( $current_page > 1 && isset($_GET["photo"]) == '' )
+				{
+					echo '<a style="color: #e3e3e3;" href="?page='.($current_page-1).'"> &lt;&lt;Zurück&nbsp</a>&nbsp';
+				} 
+			if( $current_page < $last_page && isset($_GET["photo"]) == '' )
+				{
+					echo '&nbsp<a style="color: #e3e3e3;" href="?page='.($current_page+1).'">Weiter&gt;&gt;</a> '; 
+				}
+			if( $current_page != $last_page && isset($_GET["photo"]) == '' )
+				{
+					echo '&nbsp&nbsp&nbsp<a style="color: #e3e3e3;" href="?page='.($last_page).'">Letzte Seite</a>';
+				}
+				
+				
+				echo '</div>';
+		}
+
+	//NEW - Using the following function you can create the navigation links * END
+		
 	// The $photo parameter is used to show an individual photo
+
 	$file = (isset($_GET['photo']) ? $_GET['photo'] : null);
 	if (isset($file)) {
 		$key = array_search($file, $files); // Determine the array key of the current item (we need this for generating the Next and Previous links)
@@ -227,20 +295,16 @@
 		$exif = exif_read_data($file, 0, true);
 		$filepath = pathinfo($file);
 
-		// Generate an optional short link
-		if($use_shortLink) {
-        $short_link = exec("curl '".$shortLink_API."'");
-        }else{
-        $short_link = "#";
-    }
-
 		//Check if the related RAW file exists and link to it
 		$rawfile=glob($photo_dir.$filepath['filename'].$raw_formats, GLOB_BRACE);
+		$exif = exif_read_data($file, 0, true);
+		$titel = $exif["IFD0"]["ImageDescription"];
 		if (!empty($rawfile)) {
 			echo "<h1>".$filepath['filename']." <a class='superscript' href=".$rawfile[0].">RAW</a></h1>";
 		}
 		else {
-			echo "<h1>".$filepath['filename']."</h1>";
+		// HPD: Anzeige des Titels in der Einzelanzeige als Überschrift.		
+			echo "<h1>".$titel."</h1>";
 		}
 
 		//NAVIGATION LINKS
@@ -249,21 +313,28 @@
 		$lastphoto = $files[0];
 
 		// If there is only one photo in the album, show the home navigation link
+
+		// NEW - Several changes of code to fit with sort order * START
 		if ($fileCount == 1) {
-			echo "<div class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='g'>Grid</a> &bull; </div>";
+			echo "<div class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='g'>Übersicht</a> &bull; </div>";
 		}
 		// Disable the Previous link if this is the last photo
 		elseif (empty($files[$key+1])) {
-			echo "<div class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='g'>Grid</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key-1].'&d='.$sub_photo_dir."' accesskey='n'>Next</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$lastphoto.'&d='.$sub_photo_dir."' accesskey='l'>Last</a></div>";
+			echo "<div class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='g'>Übersicht</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key-1].'&d='.$sub_photo_dir."' accesskey='n'>Weiter</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$lastphoto.'&d='.$sub_photo_dir."' accesskey='l'>Letzte Seite</a></div>";
 		}
 		// Disable the Next link if this is the first photo
 		elseif (empty($files[$key-1])) {
-			echo "<div class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='g'>Grid</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$firstphoto.'&d='.$sub_photo_dir."' accesskey='f'>First</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key+1].'&d='.$sub_photo_dir."' accesskey='p'>Previous</a></div>";
+			echo "<div class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='g'>Übersicht</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$firstphoto.'&d='.$sub_photo_dir."' accesskey='f'>Erste Seite</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key+1].'&d='.$sub_photo_dir."' accesskey='p'>Zurück</a></div>";
 		}
 		// Show all navigation links
 		else {
-			echo "<div class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='g'>Grid</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$firstphoto.'&d='.$sub_photo_dir."' accesskey='f'>First</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key+1].'&d='.$sub_photo_dir."' accesskey='p'>Previous</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key-1].'&d='.$sub_photo_dir."' accesskey='n'>Next</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$lastphoto.'&d='.$sub_photo_dir."' accesskey='l'>Last</a></div>";
+			
+			echo "<div class='center'>
+			<a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='g'>Übersicht</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$firstphoto.'&d='.$sub_photo_dir."' accesskey='f'>Erste Seite</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key+1].'&d='.$sub_photo_dir."' accesskey='p'>Zurück</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key-1].'&d='.$sub_photo_dir."' accesskey='n'>Weiter</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$lastphoto.'&d='.$sub_photo_dir."' accesskey='l'>Letzte Seite</a></div>";
+			
 		}
+		// NEW - Several changes of code to fit with sort order * END
+		
 		// Check whether the localized description file matching the browser language exists
 		if (file_exists($photo_dir.$language.'-'.$filepath['filename'].'.txt')) {
 			$description = @file_get_contents($photo_dir.$language.'-'.$filepath['filename'].'.txt');
@@ -272,7 +343,6 @@
 			$description = @file_get_contents($photo_dir.$filepath['filename'].'.txt');
 		}
 		$gps = read_gps_location($file);
-		$shortened_link = " &bull; <a href='".$short_link."'><i class='fa fa-link'></i></a> ";
 
 		$fnumber = $exif['COMPUTED']['ApertureFNumber'];
 		if (empty($fnumber) ) {
@@ -285,8 +355,16 @@
 			$exposuretime="";
 		} else {
 			$exposuretime=$exposuretime." &bull; ";
-		}
-		$iso=$exif['EXIF']['ISOSpeedRatings'];
+	}
+	// Ermitteln des Bildtitels
+	$titel = $exif['IFD0']['ImageDescription'];
+	if (empty($titel)) {
+		$titel = "";
+	} else {
+//		$titel = $titel . " &bull; "; Bullet nur interessant, wenn Fotobeschreibung aus Textdatei gelesen wird.
+		$titel = $titel;
+	}
+	$iso=$exif['EXIF']['ISOSpeedRatings'];
 		if (empty($iso)) {
 			$iso="";
 		} else {
@@ -305,21 +383,20 @@
 		}
 
 		$photo_info = $fnumber.$exposuretime.$iso.$datetime;
-		// Enable the short link anchor if short link is being used
-		if($use_shortLink){
-			$photo_info = $photo_info.$shortened_link;
-		}
 		// Enable the Map anchor if the photo contains geographical coordinate
 		if (!empty($gps[lat])) {
 			$photo_info = $photo_info.$map_url;
 		}
 
+	// Anzeige des Titels in der Bildunterschrift
+
 		$info = "<span style='word-spacing:1em'>".$photo_info."</span>";
 		// Show photo, EXIF data, description, and info
-		echo '<div class="center"><ul class="rig column-1"><li><a href="'.$file.'"><img src="'.$tim.'" alt=""></a><p class="caption">'.$exif['COMMENT']['0'].' '.$description.'</p><p class="box">'.$info.'</p></li></ul></div>';
+	echo '<div class="center"><ul class="rig column-1"><li><a href="' . $file . '"><img src="' . $tim . '" alt=""></a><p class="caption">' . $titel . ' ' . $description . '</p><p class="box">' . $info . '</p></li></ul></div>';
 	}
 
 	// Show links
+
 	if ($links) {
 		$array_length = count($links);
 		echo '<div class="footer">';
@@ -332,5 +409,8 @@
 	}
 	?>
 	<div>
+		<br> <!--NEW Added 3 br's for navigation of pagination -->
+		<br>
+		<br>
 	</body>
 </html>
